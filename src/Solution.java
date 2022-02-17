fc2486bce96ac25f27e26b29958c3a4fb137a14e
nicholasjg97 <nicholas.j.gaytan97@gmail.com>
2022-02-16 23:57:29 -0600

    solutuon needs fixing, took over 30 min

--- src/Solution.java
+++ src/Solution.java
@@ -12,4 +12,27 @@ public class Solution {
         System.out.println(b);
         System.out.println(c);
     }
+
+//    Given an integer, , perform the following conditional actions:
+//
+//If  is odd, print Weird
+//If  is even and in the inclusive range of  to , print Not Weird
+//If  is even and in the inclusive range of  to , print Weird
+//If  is even and greater than , print Not Weird
+//    private static final Scanner scanner = new Scanner(System.in);
+//
+//    public static void main(String[] args) {
+//        int N = scanner.nextInt();
+//        scanner.skip("1");
+//        do
+//            if (N >= 2 && N <= 5) {
+//                System.out.println("Not Weird");
+//            } else if (N >= 6 && N <= 20) {
+//                System.out.println("Weird");
+//            } else if (N > 20) {
+//                System.out.println("Not Weird");
+//            }
+//
+//        scanner.close();
+//    }
 }
