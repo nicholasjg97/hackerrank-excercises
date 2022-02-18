dfe3b4727074db4b1beaff70e30470a709afc290
nicholasjg97 <nicholas.j.gaytan97@gmail.com>
2022-02-17 23:53:54 -0600

    more review challenges

--- src/Solution.java
+++ src/Solution.java
@@ -11,7 +11,6 @@ public class Solution {
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
-    }
 
 //    Given an integer, , perform the following conditional actions:
 //
@@ -23,16 +22,38 @@ public class Solution {
 //
 //    public static void main(String[] args) {
 //        int N = scanner.nextInt();
-//        scanner.skip("1");
-//        do
-//            if (N >= 2 && N <= 5) {
-//                System.out.println("Not Weird");
-//            } else if (N >= 6 && N <= 20) {
-//                System.out.println("Weird");
-//            } else if (N > 20) {
-//                System.out.println("Not Weird");
-//            }
-//
-//        scanner.close();
+//       if(N%2==1)
+//        {
+//            System.out.println("Weird");
+//        }
+//        else if(N>=2 && N<=5){
+//            System.out.println("Not Weird");
+//        }
+//        else if(N>=6 && N<=20)
+//        {
+//            System.out.println("Weird");
+//        }
+//        else
+//        {
+//            System.out.println("Not Weird");
+//        }
 //    }
+
+//    There are three lines of output:
+//
+//    On the first line, print String: followed by the unaltered String read from stdin.
+//    On the second line, print Double: followed by the unaltered double read from stdin.
+//    On the third line, print Int: followed by the unaltered integer read from stdin.
+
+//    Scanner scanner = new Scanner(System.in);
+//
+//    int i = scanner.nextInt();
+//    double d = scanner.nextDouble();
+//            scanner.nextLine();
+//    String s = scanner.nextLine();
+//
+//            System.out.println("String: "+s);
+//            System.out.println("Double: "+d);
+//            System.out.println("Int: "+i);
+    }
 }
