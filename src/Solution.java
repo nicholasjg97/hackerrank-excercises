7e6f2110e675bd647941842d6e9403a9fa72ba8c
nicholasjg97 <nicholas.j.gaytan97@gmail.com>
2022-02-19 23:58:17 -0600

    added new challenge

--- src/Solution.java
+++ src/Solution.java
@@ -55,5 +55,30 @@ public class Solution {
 //            System.out.println("String: "+s);
 //            System.out.println("Double: "+d);
 //            System.out.println("Int: "+i);
+
+
+
+//        Input Format
+//
+//      Every line of input will contain a String followed by an integer.
+//      Each String will have a maximum of  alphabetic characters, and each integer will be in the inclusive range from  to .
+//
+//      Output Format
+//
+//      In each line of output there should be two columns:
+//      The first column contains the String and is left justified using exactly  characters.
+//      The second column contains the integer, expressed in exactly  digits; if the original input has less than three digits, you must pad your output's leading digits with zeroes.
+
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.println("================================");
+//        for (int x = 0; x < 3; x++) {
+//            String s = scan.next();
+//            int i = scan.nextInt();
+//
+//            System.out.printf("%-14s %03d\n", s, i);
+//        }
+//
+//        System.out.println("================================");
     }
 }
